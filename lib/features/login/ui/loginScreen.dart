b912b4985dbddbd59271ce137771b996e40da7a0
Abdelhamid shahin <abdelhamid.shahin0@gmail.com>
2025-10-01 14:07:10 +0300

    fake puch

--- lib/features/login/ui/loginScreen.dart
+++ lib/features/login/ui/loginScreen.dart
@@ -5,6 +5,6 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("login_creen"));
+    return Center(child: Text("login_screen"));
   }
 }
