946853c4a2cbb40e7eea0cde2b4af0de7855f718
Abdelhamid shahin <abdelhamid.shahin0@gmail.com>
2025-10-01 13:38:14 +0300

    fake push text 2

--- lib/features/login/ui/loginScreen.dart
+++ lib/features/login/ui/loginScreen.dart
@@ -5,7 +5,7 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("login "));
+    return Center(child: Text("login screen "));
   }
 }
 
